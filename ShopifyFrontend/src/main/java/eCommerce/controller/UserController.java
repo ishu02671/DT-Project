f7704b9d556e36c6324976b0b67f2095f7d2c05b
ishu02671 <ishu02671@gmail.com>
2019-01-22 20:15:06 +0530

    Added UserControl

--- ShopifyFrontend/src/main/java/eCommerce/controller/UserController.java
+++ ShopifyFrontend/src/main/java/eCommerce/controller/UserController.java
@@ -1,7 +1,13 @@
 package eCommerce.controller;
 
+import java.util.Collection;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +20,37 @@ public class UserController
 	public String loginCheck(Model m, HttpSession session)
 	{
 		String page="";
+		
+		boolean loggedIn=false;
+		
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		
+		String username = authentication.getName();
+		
+		Collection<GrantedAuthority> roles = (Collection<GrantedAuthority>)authentication.getAuthorities();
+		
+		for(GrantedAuthority role:roles)
+		{
+			session.setAttribute("role", role.getAuthority());
+			
+			if(role.getAuthority().equals("ROLE_ADMIN"))
+			{
+				loggedIn=true;
+				page="AdminHome";
+				session.setAttribute("loggedIn", loggedIn);
+				session.setAttribute("username", username);
+			}
+			else
+			{
+				loggedIn=true;
+				page="UserHome";
+				session.setAttribute("loggedIn", loggedIn);
+				session.setAttribute("username", username);
+				
+			}
+		}
+		
 		return page;
 		
 	}
